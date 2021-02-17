8bd339e50f30e8a1d0af041cd969c24adf68903f
erica dreisbach <erica@ericadreisbach.com>
2021-02-17 16:39:56 -0600

    readding template directory TDIR variable

--- wp-content/themes/rac-theme/functions.php
+++ wp-content/themes/rac-theme/functions.php
@@ -5,3 +5,15 @@
 
 // 0. set up a constant to the template directory to avoid extra queries to DB
 define('TDIR', get_bloginfo('template_directory'));
+
+
+// 1. For debugging - output all scripts
+/*
+function inspect_scripts() {
+    global $wp_scripts;
+    foreach( $wp_scripts->queue as $handle ) :
+        echo $handle . ' | ';
+    endforeach;
+}
+*/
+//add_action( 'wp_print_scripts', 'inspect_scripts', 99 );
