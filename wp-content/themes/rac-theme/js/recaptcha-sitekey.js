46394f6357629bfeca5ed066cf739683819d51af
erica dreisbach <erica@ericadreisbach.com>
2021-02-26 11:10:31 -0600

    fixes to nav

--- wp-content/themes/rac-theme/js/recaptcha-sitekey.js
+++ wp-content/themes/rac-theme/js/recaptcha-sitekey.js
@@ -2,30 +2,12 @@
 // get sitekey at https://www.google.com/recaptcha/admin
 // src https://developers.google.com/recaptcha/docs/v3
 
+// with form id
 function onSubmit(token) {
 	document.getElementById("loginform").submit();
 }
 
+// with button id
 jQuery(document).ready(function(){
 	jQuery('#wp-submit').addClass('g-recaptcha').attr('data-sitekey', '6LdyE14aAAAAAAsiHd0-dYzpQ3G2vrz81wvROiSt').attr('data-callback', 'onSubmit').attr('data-action', 'submit');
 });
-
-
-
-
-
-
-// reCAPTCHA v2
-// get sitekeys at https://www.google.com/recaptcha/admin
-
-/*
-var recaptcha = '<div class="g-recaptcha" data-sitekey="XXXXXX" data-callback="enableBtn"></div>';
-jQuery(document).ready(function(){
-	jQuery(recaptcha).insertBefore(jQuery('#loginform'));
-});
-*/
-
-
-// disable Log In button until user passes reCAPTCHA
-// document.getElementById("wp-submit").disabled = true;
-// function enableBtn(){ document.getElementById('wp-submit').disabled = false; }
