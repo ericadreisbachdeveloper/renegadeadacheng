7e7bfcd8badb417c2ae134c702774814444fd97f
erica dreisbach <erica@ericadreisbach.com>
2021-02-26 16:24:05 -0600

    added label to search form

--- wp-content/themes/rac-theme/searchform.php
+++ wp-content/themes/rac-theme/searchform.php
@@ -6,6 +6,7 @@
 
 	<a href="#display-search" role="button" aria-pressed="false" aria-label="Show/Hide Search Form"></a>
 
+	<label for="nav-search" class="sr-only">Search </label>
 	<input id="nav-search" class="search-input" type="search" name="s" placeholder="<?php _e( 'Search', 'dbllc' ); ?>">
 	<button class="search-submit" type="submit" aria-label="Search This Query"></button>
 
