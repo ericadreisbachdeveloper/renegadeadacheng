65e93f611ed1027d08cafbfafc7561ccd2f83ab7
erica dreisbach <erica@ericadreisbach.com>
2021-05-01 00:50:37 -0500

    trying query

--- files/themes/rac-theme/header.php
+++ files/themes/rac-theme/header.php
@@ -240,7 +240,8 @@ ob_start('sanitize_output'); ?>
 
 
 <!-- Global site tag (gtag.js) - Google Analytics - conditionally hidden from PageSpeed Insights -->
-<?php if($_SERVER['HTTP_USER_AGENT']) { _e($_SERVER['HTTP_USER_AGENT']); } ?>
+<?php if($_SERVER['HTTP_USER_AGENT'] && stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false) { _e($_SERVER['HTTP_USER_AGENT']); } ?>
+
 <?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-QHVPBX71VD"></script>
 <script>
