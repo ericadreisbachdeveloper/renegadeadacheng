5cc339b8130a0e2c9497e1580db39e73a8728370
erica dreisbach <erica@ericadreisbach.com>
2021-05-01 22:58:24 -0500

    making pagination_from_menu conditional

--- files/themes/rac-theme/footer.php
+++ files/themes/rac-theme/footer.php
@@ -10,6 +10,43 @@
 	<?php if(is_single()) { include(locate_template('template/pagination_single.php')); } ?>
 
 
+	<!-- show page-to-page navigation -->
+	<?php if(!is_front_page()) { include(locate_template('template/pagination_from_menu.php')); }  ?>
+
+
+
+	<div class="container-fluid footer-social-container">
+		<style>
+			.footer-social-container .container { height: 26.086rem; }
+			@media (min-width: 62rem) { .footer-social-container .container { height: auto; } }
+		</style>
+		<div class="container">
+			<div class="row footer-widgets-row">
+				<?php if(is_active_sidebar('Footer Widgets')) { dynamic_sidebar( 'Footer Widgets' ); } ?>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container-fluid footer-brand-container">
+		<style>
+			.footer-brand-container .container { height: 8.749rem; }
+			@media (min-width: 62rem) { .footer-brand-container .container { height: auto; } }
+		</style>
+		<div class="container">
+			<div class="row footer-menus-row">
+
+				<!-- logo -->
+				<div class="col-sm-6">
+					<a href="<?= esc_url(get_home_url()); ?>" class="footer-logo-a" title="Renegade Ada Cheng | Chicago-based Taiwanese Storyteller, Producer, Speaker | Home"></a>
+				</div>
+
+				<!-- copyright -->
+				<?php if(is_active_sidebar('Copyright')) { dynamic_sidebar('Copyright'); } ?>
+
+			</div>
+		</div>
+	</div>
 
 
 </footer>
