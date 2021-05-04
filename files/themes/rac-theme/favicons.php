82c8659fb886e394bb24878d5922a70ff3b22961
erica dreisbach <erica@ericadreisbach.com>
2021-05-03 22:56:28 -0500

    simplified favicons.php

--- files/themes/rac-theme/favicons.php
+++ files/themes/rac-theme/favicons.php
@@ -5,16 +5,10 @@
 <meta name="msapplication-TileColor" content="#FFFFFF">
 <meta name="msapplication-TileImage" content="<?php _e(TDIR); ?>/favicons/favicon-144.png">
 
-<meta name="application-name" content="Name">
-<meta name="msapplication-tooltip" content="Tooltip">
-<meta name="msapplication-config" content="ieconfig.xml">
-
-<!-- <link href="<?php ?>/favicon.ico" type="image/x-icon" rel="shortcut icon"> -->
 
 <link rel="icon" href="<?php _e(TDIR); ?>/favicons/favicon-32.png" type="image/png" sizes="32x32">
 
 <link rel="apple-touch-icon-precomposed" sizes="72x72" type="image/png" href="<?php _e(TDIR); ?>/favicons/favicon-72.png">
 <link rel="apple-touch-icon-precomposed" sizes="114x114" type="image/png" href="<?php _e(TDIR); ?>/favicons/favicon-114.png">
 <link rel="apple-touch-icon-precomposed" sizes="120x120" type="image/png" href="<?php _e(TDIR); ?>/favicons/favicon-120.png">
-<link rel="apple-touch-icon-precomposed" type="image/png" href="<?php _e(TDIR); ?>/favicons/favicon-152.png">
 <link rel="shortcut icon" sizes="196x196" type="image/png" href="<?php _e(TDIR); ?>/favicons/favicon-196.png">
